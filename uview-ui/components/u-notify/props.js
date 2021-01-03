f516845d094698196d16d903e7cecd53dfdf972a
yiruiwen <1416956117@qq.com>
2021-01-03 19:07:27 +0800

    完成notify组件

--- uview-ui/components/u-notify/props.js
+++ uview-ui/components/u-notify/props.js
@@ -3,7 +3,14 @@ export default {
 		// 到顶部的距离
 		top: {
 			type: [Number, String],
+			// #ifdef H5
+			// H5端，导航栏为普通元素，需要将组件移动到导航栏的下边沿
+			// H5的导航栏高度为44px
+			default: 44
+			// #endif
+			// #ifndef H5
 			default: 0
+			// #endif
 		},
 		// 是否展示组件
 		show: {
@@ -23,7 +30,7 @@ export default {
 		// 背景颜色
 		bgColor: {
 			type: String,
-			default: '#2979ff'
+			default: ''
 		},
 		// 展示的文字内容
 		message: {
--- uview-ui/components/u-notify/u-notify.vue
+++ uview-ui/components/u-notify/u-notify.vue
@@ -1,19 +1,15 @@
 <template>
 	<u-transition
 	    mode="slide-down"
-	    :customStyle="{
-		top: $u.addUnit(top)
-	}"
-	    :show="show"
+	    :customStyle="containerStyle"
+	    :show="showStatus"
 	>
-		<view class="u-notify" :style="{
-			backgroundColor: bgColor
-		}">
+		<view class="u-notify" :class="[`u-notify--${type}`]" :style="[backgroundColor]">
 			<text
 			    class="u-notify__text"
 			    :style="{
 					fontSize: $u.addUnit(fontSize),
-					color: color
+					color: color 
 				}"
 			>{{ message }}</text>
 		</view>
@@ -27,15 +23,90 @@
 		mixins: [uni.$u.mixin, props],
 		data() {
 			return {
-
+				timer: null,
+				showStatus: false
+			}
+		},
+		watch: {
+			show: {
+				immediate: true,
+				handler(n) {
+					// 根据true或者false，开启或者关闭组件
+					n === true ? this.open() : this.close()
+				}
+			}
+		},
+		computed: {
+			containerStyle() {
+				const style = {
+					top: uni.$u.addUnit(this.top),
+					// 因为组件底层为u-transition组件，必须将其设置为fixed定位
+					// 让其出现在导航栏底部
+					position: 'fixed', 
+					left: 0,
+					right: 0
+				}
+				return style
+			},
+			// 组件背景颜色
+			backgroundColor() {
+				const style = {}
+				if(this.bgColor) {
+					style.backgroundColor = this.bgColor
+				}
+				return style
 			}
 		},
 		methods: {
-			
+			open() {
+				// 任何定时器初始化之前，都要执行清除操作，否则可能会造成混乱
+				clearTimeout(this.timer)
+				this.timer = null
+				if(this.duration > 0 && this.duration !== Infinity) {
+					// 等待组件创建完毕
+					this.$nextTick(function(){
+						this.showStatus = true
+					})
+					// 定时结束后，关闭组件
+					this.timer = setTimeout(() => {
+					    this.close()
+					}, this.duration)
+				}
+			},
+			// 关闭notify
+			close() {
+				clearTimeout(this.timer)
+				this.timer = null
+				// 标记组件为关闭状态
+				this.showStatus = false
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
-	.u-notify {}
+	.u-notify {
+		padding: 8px 0;
+		
+		&__text {
+			font-size: 15px;
+			text-align: center;
+		}
+		
+		&--primary { 
+			background-color: $u-primary;
+		}
+		
+		&--success {
+			background-color: $u-success;
+		}
+		
+		&--error {
+			background-color: $u-error;
+		}
+		
+		&--warning {
+			background-color: $u-warning;
+		}
+	}
 </style>

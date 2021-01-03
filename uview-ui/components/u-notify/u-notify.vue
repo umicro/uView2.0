3414b481230e9e234990887986e523b5e8229dcb
hyx6fly <hyx6fly@gmail.com>
2021-01-03 20:59:24 +0800

    notify事列、scss变量命名、文档注释

--- uview-ui/components/u-notify/u-notify.vue
+++ uview-ui/components/u-notify/u-notify.vue
@@ -4,7 +4,11 @@
 	    :customStyle="containerStyle"
 	    :show="showStatus"
 	>
-		<view class="u-notify" :class="[`u-notify--${type}`]" :style="[backgroundColor]">
+		<view
+		    class="u-notify"
+		    :class="[`u-notify--${type}`]"
+		    :style="[backgroundColor]"
+		>
 			<text
 			    class="u-notify__text"
 			    :style="{
@@ -18,6 +22,20 @@
 
 <script>
 	import props from './props.js'
+	/**
+	 * notify 顶部提示
+	 * @description 该组件一般用于页面顶部向下滑出一个提示，尔后自动收起的场景
+	 * @tutorial
+	 * @property {String Number} top 到顶部的距离
+	 * @property {Boolean} show 是否展示组件
+	 * @property {String} type主题，primary，success，warning，error
+	 * @property {String} color 字体颜色
+	 * @property {String} bgColor 背景颜色
+	 * @property {String} message 展示的文字内容
+	 * @property {String} duration 展示时长，为0时不消失，单位ms
+	 * @property {String} fontSize  字体大小
+	 * @example <u-upload :action="action" :fileList="fileList" ></u-upload>
+	 */
 	export default {
 		name: 'u-notify',
 		mixins: [uni.$u.mixin, props],
@@ -42,7 +60,7 @@
 					top: uni.$u.addUnit(this.top),
 					// 因为组件底层为u-transition组件，必须将其设置为fixed定位
 					// 让其出现在导航栏底部
-					position: 'fixed', 
+					position: 'fixed',
 					left: 0,
 					right: 0
 				}
@@ -51,9 +69,10 @@
 			// 组件背景颜色
 			backgroundColor() {
 				const style = {}
-				if(this.bgColor) {
+				if (this.bgColor) {
 					style.backgroundColor = this.bgColor
 				}
+				console.log(style);
 				return style
 			}
 		},
@@ -62,14 +81,14 @@
 				// 任何定时器初始化之前，都要执行清除操作，否则可能会造成混乱
 				clearTimeout(this.timer)
 				this.timer = null
-				if(this.duration > 0 && this.duration !== Infinity) {
+				if (this.duration > 0 && this.duration !== Infinity) {
 					// 等待组件创建完毕
-					this.$nextTick(function(){
+					this.$nextTick(function() {
 						this.showStatus = true
 					})
 					// 定时结束后，关闭组件
 					this.timer = setTimeout(() => {
-					    this.close()
+						this.close()
 					}, this.duration)
 				}
 			},
@@ -85,28 +104,36 @@
 </script>
 
 <style lang="scss">
+	$u-notify-padding: 8px 0 !default;
+	$u-notify-text-font-size: 15px !default;
+	$u-notify-primary-bgColor: $u-primary !default;
+	$u-notify-success-bgColor: $u-success !default;
+	$u-notify-error-bgColor: $u-error !default;
+	$u-notify-warning-bgColor: $u-warning !default;
+
+
 	.u-notify {
-		padding: 8px 0;
-		
+		padding: $u-notify-padding;
+
 		&__text {
-			font-size: 15px;
+			font-size: $u-notify-text-font-size;
 			text-align: center;
 		}
-		
-		&--primary { 
-			background-color: $u-primary;
+
+		&--primary {
+			background-color: $u-notify-primary-bgColor;
 		}
-		
+
 		&--success {
-			background-color: $u-success;
+			background-color: $u-notify-success-bgColor;
 		}
-		
+
 		&--error {
-			background-color: $u-error;
+			background-color: $u-notify-error-bgColor;
 		}
-		
+
 		&--warning {
-			background-color: $u-warning;
+			background-color: $u-notify-warning-bgColor;
 		}
 	}
 </style>

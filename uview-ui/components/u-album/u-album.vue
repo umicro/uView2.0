48aa33d6ab31653af66f92fff4d2ff01a70511ba
yiruiwen <1416956117@qq.com>
2021-02-17 04:22:07 +0800

    新增album相册组件，尚未完成

---
+++ uview-ui/components/u-album/u-album.vue
@@ -0,0 +1,38 @@
+<template>
+	<view class="u-album">
+		<view
+		    class="u-album__wrapper"
+		    v-for="(item, index) in urls"
+		    :key="index"
+		>
+			<image
+			    :src="$u.test.object(item) ? (key && item[key] || item.url) : item"
+			    :mode="urls.length === 1 ? singleMode : multipleMode"
+			    @tap.stop="onPreviewTap"
+			></image>
+		</view>
+	</view>
+</template>
+
+<script>
+	import props from './props.js'
+	export default {
+		name: 'u-album',
+		mixins: [uni.$u.mixin, props],
+		data() {
+			return {
+
+			}
+		},
+		// 预览图片
+		onPreviewTap() {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	@import "../../libs/css/components.scss";
+
+	.u-album {}
+</style>

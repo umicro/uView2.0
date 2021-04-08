5fe75a58a8c11a106d01d4408ade93d5884c9546
yiruiwen <1416956117@qq.com>
2021-04-08 23:23:34 +0800

    完善calender日历

---
+++ uview-ui/components/u-calendar/header.vue
@@ -0,0 +1,81 @@
+<template>
+	<view class="u-calendar-header">
+		<text class="u-calendar-header__title">{{ title }}</text>
+		<text class="u-calendar-header__subtitle">{{ subtitle }}</text>
+		<view class="u-calendar-header__weekdays">
+			<text class="u-calendar-header__weekday">一</text>
+			<text class="u-calendar-header__weekday">二</text>
+			<text class="u-calendar-header__weekday">三</text>
+			<text class="u-calendar-header__weekday">四</text>
+			<text class="u-calendar-header__weekday">五</text>
+			<text class="u-calendar-header__weekday">六</text>
+			<text class="u-calendar-header__weekday">日</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: 'u-calendar-header',
+		mixins: [uni.$u.mixin],
+		props: {
+			// 标题
+			title: {
+				type: String,
+				default: ''
+			},
+			// 副标题
+			subtitle: {
+				type: String,
+				default: ''
+			},
+			// 是否显示先标题
+			showTitle: {
+				type: Boolean,
+				default: true
+			},
+			// 是否显示先标题
+			showTitle: {
+				type: Boolean,
+				default: true
+			},
+		},
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			name() {
+				
+			}
+		},
+	}
+</script>
+
+<style lang="scss">
+	@import "../../libs/css/components.scss";
+	
+	.u-calendar-header {
+		
+		&__title {
+			font-size: 16px;
+			color: $u-main-color;
+		}
+		
+		&__subtitle {
+			font-size: 14px;
+			color: $u-content-color;
+		}
+		
+		&__weekdays {
+			@include flex;
+			
+			&__weekday {
+				font-size: 14px;
+				color: $u-main-color;
+				justify-content: space-between;
+			}
+		}
+	}
+</style>
---
+++ uview-ui/components/u-calendar/month.vue
@@ -0,0 +1,30 @@
+<template>
+	<view class="u-calendar-month">
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		name: 'u-calendar-month',
+		mixins: [uni.$u.mixin],
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			name() {
+				
+			}
+		},
+	}
+</script>
+
+<style lang="scss">
+	@import "../../libs/css/components.scss";
+	
+	.u-calendar-month {
+		
+	}
+</style>

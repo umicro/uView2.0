0d6f341f51a85c4eed7529892ee36c7ddece6534
yiruiwen <1416956117@qq.com>
2021-07-30 11:43:19 +0800

    upload组件无法触发上传的问题

---
+++ uview-ui/components/u-drawdown/u-drawdown.vue
@@ -0,0 +1,29 @@
+<template>
+	<view class="u-drawdown">
+		
+	</view>
+</template>
+
+<script>
+	import props from './props.js'
+	export default {
+		name: 'u-drawdown',
+		mixins: [uni.$u.mixin, props],
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			name() {
+				
+			}
+		},
+	}
+</script>
+
+<style lang="scss">
+	@import '../../libs/css/components.scss';
+	
+	
+</style>

42a47a58dd7daccfbe36bc17b1d82dad272a8b91
北桥 <tonghao1222@qq.com>
2021-11-15 23:49:31 +0800

    处理formatter

--- uni_modules/uview-ui/libs/config/props/textarea.js
+++ uni_modules/uview-ui/libs/config/props/textarea.js
@@ -8,29 +8,29 @@
  * @FilePath     : /u-view2.0/uview-ui/libs/config/props/textarea.js
  */
 export default {
-    // textarea 组件
-    textarea: {
-        value: '',
-        placeholder: '',
-        placeholderClass: 'textarea-placeholder',
-        placeholderStyle: 'color: #c0c4cc',
-        height: 70,
-        confirmType: 'done',
-        disabled: false,
-        count: false,
-        focus: false,
-        autoHeight: false,
-        fixed: false,
-        cursorSpacing: 0,
-        cursor: '',
-        showConfirmBar: true,
-        selectionStart: -1,
-        selectionEnd: -1,
-        adjustPosition: true,
-        disableDefaultPadding: false,
-        holdKeyboard: false,
-        maxlength: 140,
-        border: 'surround',
+	// textarea 组件
+	textarea: {
+		value: '',
+		placeholder: '',
+		placeholderClass: 'textarea-placeholder',
+		placeholderStyle: 'color: #c0c4cc',
+		height: 70,
+		confirmType: 'done',
+		disabled: false,
+		count: false,
+		focus: false,
+		autoHeight: false,
+		fixed: false,
+		cursorSpacing: 0,
+		cursor: '',
+		showConfirmBar: true,
+		selectionStart: -1,
+		selectionEnd: -1,
+		adjustPosition: true,
+		disableDefaultPadding: false,
+		holdKeyboard: false,
+		maxlength: 140,
+		border: 'surround',
 		formatter: null
-    }
+	}
 }

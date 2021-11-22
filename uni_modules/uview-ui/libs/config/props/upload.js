3305e520543243d45b441540be801171174c4b0a
北桥 <tonghao1222@qq.com>
2021-11-22 20:51:13 +0800

    添加uploadIconColor默认值

--- uni_modules/uview-ui/libs/config/props/upload.js
+++ uni_modules/uview-ui/libs/config/props/upload.js
@@ -8,28 +8,29 @@
  * @FilePath     : /u-view2.0/uview-ui/libs/config/props/upload.js
  */
 export default {
-    // upload组件
-    upload: {
-        accept: 'image',
-        capture: () => ['album', 'camera'],
-        compressed: true,
-        camera: 'back',
-        maxDuration: 60,
-        uploadIcon: 'camera-fill',
-        useBeforeRead: false,
-        previewFullImage: true,
-        maxCount: 52,
-        disabled: false,
-        imageMode: 'aspectFill',
-        name: '',
-        sizeType: () => ['original', 'compressed'],
-        multiple: false,
-        deletable: true,
-        maxSize: Number.MAX_VALUE,
-        fileList: () => [],
-        uploadText: '',
-        width: 80,
-        height: 80,
-        previewImage: true
-    }
+	// upload组件
+	upload: {
+		accept: 'image',
+		capture: () => ['album', 'camera'],
+		compressed: true,
+		camera: 'back',
+		maxDuration: 60,
+		uploadIcon: 'camera-fill',
+		uploadIconColor: '#D3D4D6',
+		useBeforeRead: false,
+		previewFullImage: true,
+		maxCount: 52,
+		disabled: false,
+		imageMode: 'aspectFill',
+		name: '',
+		sizeType: () => ['original', 'compressed'],
+		multiple: false,
+		deletable: true,
+		maxSize: Number.MAX_VALUE,
+		fileList: () => [],
+		uploadText: '',
+		width: 80,
+		height: 80,
+		previewImage: true
+	}
 }

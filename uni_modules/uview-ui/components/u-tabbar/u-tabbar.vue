6bc755a64f9772b39321344c23f193de9e9d2dba
朱贤 <18270244870@163.com>
2021-12-14 17:48:41 +0800

    fixed: 修复tabbar组件开启safeAreaInsetBottom属性后，placeholder高度填充不正确

--- uni_modules/uview-ui/components/u-tabbar/u-tabbar.vue
+++ uni_modules/uview-ui/components/u-tabbar/u-tabbar.vue
@@ -94,7 +94,7 @@
 				// 延时一定时间
 				await uni.$u.sleep(20)
 				// #ifndef APP-NVUE
-				this.$uGetRect('.u-tabbar__content').then(({height = 0}) => {
+				this.$uGetRect('.u-tabbar__content').then(({height = 50}) => {
 					// 修复IOS safearea bottom 未填充高度
 					this.placeholderHeight = height
 				})

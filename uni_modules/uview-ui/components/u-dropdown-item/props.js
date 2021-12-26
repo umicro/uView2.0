0e6d4f54d07e228268e9c39c7528dd829fdb1d78
iloomo <yanghailong@iloomo.com>
2021-12-26 17:06:34 +0800

    将dropdown1.0适配2.0

--- uni_modules/uview-ui/components/u-dropdown-item/props.js
+++ uni_modules/uview-ui/components/u-dropdown-item/props.js
@@ -1,33 +1,38 @@
 export default {
     props: {
-        // 当前选中项的value值
+		//2.0
+		top: {
+			type:String,
+			default:'126px'
+		},
+        // 当前选中项的value值 1.0 2.0
         value: {
             type: [Number, String, Array],
             default: ''
         },
-        // 菜单项标题
+        // 菜单项标题 1.0 2.0
         title: {
             type: [String, Number],
             default: ''
         },
-        // 选项数据，如果传入了默认slot，此参数无效
+        // 选项数据，如果传入了默认slot，此参数无效 1.0 2.0
         options: {
             type: Array,
             default() {
                 return []
             }
         },
-        // 是否禁用此菜单项
+        // 是否禁用此菜单项 1.0 2.0
         disabled: {
             type: Boolean,
             default: false
         },
-        // 下拉弹窗的高度
+        // 下拉弹窗的高度 1.0 2.0
         height: {
             type: [Number, String],
             default: 'auto'
         },
-        // 点击遮罩是否可以收起弹窗
+        // 点击遮罩是否可以收起弹窗 2.0
         closeOnClickOverlay: {
             type: Boolean,
             default: true

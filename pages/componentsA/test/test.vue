cd586344afcb9194396d0a38b3bd29f284d66a01
yiruiwen <1416956117@qq.com>
2021-12-30 12:56:45 +0800

    picker选项添加默认颜色

--- pages/componentsA/test/test.vue
+++ pages/componentsA/test/test.vue
@@ -1,5 +1,33 @@
 <template>
 	<view>
-		<u-icon name="map"></u-icon>
+		<u-list style="height: 500px; background-color: red;">
+			<u-list-item>
+				<u-image src="https://img2020.cnblogs.com/blog/35695/202112/35695-20211222112522991-1769312387.jpg"></u-image>
+			</u-list-item>
+			<u-list-item>
+				<u-image src="https://img2020.cnblogs.com/blog/35695/202112/35695-20211222112522991-1769312387.jpg"></u-image>
+			</u-list-item>
+			<u-list-item>
+				<u-image src="https://img2020.cnblogs.com/blog/35695/202112/35695-20211222112522991-1769312387.jpg"></u-image>
+			</u-list-item>
+			<u-list-item>
+				<u-image src="https://img2020.cnblogs.com/blog/35695/202112/35695-20211222112522991-1769312387.jpg"></u-image>
+			</u-list-item>
+			<u-list-item>
+				<u-image src="https://img2020.cnblogs.com/blog/35695/202112/35695-20211222112522991-1769312387.jpg"></u-image>
+			</u-list-item>
+			<u-list-item>
+				<u-image src="https://img2020.cnblogs.com/blog/35695/202112/35695-20211222112522991-1769312387.jpg"></u-image>
+			</u-list-item>
+			<u-list-item>
+				<u-image src="https://img2020.cnblogs.com/blog/35695/202112/35695-20211222112522991-1769312387.jpg"></u-image>
+			</u-list-item>
+		</u-list>
 	</view>
 </template>
+
+<script>
+	export default {
+		
+	}
+</script>

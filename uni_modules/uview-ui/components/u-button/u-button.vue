f294de164bcd4bab41583881e3ddd24bf008bbe6
xuzhendong666 <810792608@Outlook.com>
2022-03-06 08:49:41 +0800

    feat: 优化节流写法

--- uni_modules/uview-ui/components/u-button/u-button.vue
+++ uni_modules/uview-ui/components/u-button/u-button.vue
@@ -112,7 +112,7 @@
 import button from "../../libs/mixin/button.js";
 import openType from "../../libs/mixin/openType.js";
 import props from "./props.js";
-import { newThrottle } from '../../libs/function/throttle';
+import throttle from '../../libs/function/throttle';
 /**
  * button 按钮
  * @description Button 按钮
@@ -168,12 +168,6 @@ export default {
         return {};
     },
     computed: {
-        throttleHandler() {
-            return newThrottle(
-                () => this.$emit("click"),
-                this.throttleTime,
-            )
-        },
         // 生成bem风格的类名
         bemClass() {
             // this.bem为一个computed变量，在mixin中
@@ -272,7 +266,7 @@ export default {
             // 非禁止并且非加载中，才能点击
             if (!this.disabled && !this.loading) {
 				// 进行节流控制，每this.throttle毫秒内，只在开始处执行
-				this.throttleHandler()
+                this.throttle(() => this.$emit("click"), this.throttleTime)
             }
         },
         // 下面为对接uniapp官方按钮开放能力事件回调的对接
--- uni_modules/uview-ui/libs/function/throttle.js
+++ uni_modules/uview-ui/libs/function/throttle.js
@@ -1,48 +1,45 @@
-let timer; let
-    flag
-/**
- * 节流原理：在一定时间内，只能触发一次
- *
- * @param {Function} func 要执行的回调函数
- * @param {Number} wait 延时的时间
- * @param {Boolean} immediate 是否立即执行
- * @return null
- */
-function throttle(func, wait = 500, immediate = true) {
-    if (immediate) {
-        if (!flag) {
-            flag = true
-            // 如果是立即执行，则在wait毫秒内开始时执行
-            typeof func === 'function' && func()
-            timer = setTimeout(() => {
-                flag = false
-            }, wait)
-        }
-    } else if (!flag) {
-        flag = true
-        // 如果是非立即执行，则在wait毫秒内的结束处执行
-        timer = setTimeout(() => {
-            flag = false
-            typeof func === 'function' && func()
-        }, wait)
-    }
-}
-export default throttle
+import md5 from './md5.min.js'
 
-export const newThrottle = (func, wait = 500, immediate = true) => {
-    let pendding = false
-    return (...args) => {
-        if (!pendding) {
-            pendding = true
-            if (immediate) {
-                func?.(...args)
-                setTimeout(() => pendding = false, wait)
-            } else {
-                setTimeout(() => {
-                    func?.(...args)
-                    pendding = false
-                }, wait)
-            }
-        }
+const throttleStore = {}
+
+const generateThrottle = (hash = '') => {
+  let pendding = false
+  const argsRef = {}
+  return (func = () => {}, wait = 500, immediate = true, ...args) => {
+
+    if (!pendding) {
+      pendding = true
+      if (immediate) {
+        func?.(...args)
+        setTimeout(() => {
+          pendding = false
+          delete throttleStore[hash]
+          console.log(throttleStore)
+        }, wait)
+      } else {
+        setTimeout(() => {
+            func?.(...args)
+            pendding = false
+            delete throttleStore[hash]
+        }, wait)
+      }
     }
+  }
+}
+
+export default function throttle (...args) {
+  const { stack } = new Error('generate throttle error')
+  const hash = md5(stack)
+  const throttleCb = throttleStore[hash]
+
+  if (throttleCb) {
+    throttleCb()
+    return
+  }
+
+  const cb = generateThrottle(hash)
+
+  throttleStore[hash] = cb
+
+  cb(...args)
 }

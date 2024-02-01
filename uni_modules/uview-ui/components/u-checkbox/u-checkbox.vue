7c6cdf79d0ea09516578d19bda3624f3cde8a930
hutravel <47356312+hutravel@users.noreply.github.com>
2024-02-01 15:04:25 +0800

    Update u-checkbox.vue inactiveColor属性需要设置在checkbox背景色上，目前只是应用于border-color上
    Update u-checkbox.vue inactiveColor属性需要设置在checkbox背景色上，目前只是应用于border-color上

--- uni_modules/uview-ui/components/u-checkbox/u-checkbox.vue
+++ uni_modules/uview-ui/components/u-checkbox/u-checkbox.vue
@@ -104,7 +104,7 @@
 			// 组件选未中激活时的颜色
 			elInactiveColor() {
 				return this.inactiveColor ? this.inactiveColor : (this.parentData.inactiveColor ? this.parentData.inactiveColor :
-					'#c8c9cc');
+					'#FFFFFF');
 			},
 			// label的颜色
 			elLabelColor() {
@@ -150,7 +150,6 @@
 				// checkbox的整体样式
 				const style = {}
 				style.backgroundColor = this.isChecked && !this.elDisabled ? this.elActiveColor : this.elInactiveColor
-				style.borderColor = this.isChecked && !this.elDisabled ? this.elActiveColor : this.elInactiveColor
 				style.width = uni.$u.addUnit(this.elSize)
 				style.height = uni.$u.addUnit(this.elSize)
 				// 如果是图标在右边的话，移除它的右边距
